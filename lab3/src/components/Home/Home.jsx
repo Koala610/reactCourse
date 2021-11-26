dd2f241be173016cd7eaa7a0241a90358b4e3d55
Vladlen <vladli2000610@gmail.com>
2021-11-26 21:25:49 +0600

    Super last commit

--- lab3/src/components/Home/Home.jsx
+++ lab3/src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
+import './Home.css';
 export default function Home({user}) {
 
   return (
     <div>
       <h1>Hi {user.name !== "" ? user.name : "guest"}! This is my site!</h1>
-      <h2>To access functionality log into your account</h2>
+      <h2 className={"header-user".repeat(user.id > -1 ? 1: 0)}>To access functionality log into your account</h2>
     </div>
     
   )
